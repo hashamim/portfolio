870b16c88df6d6c6085244e176c34ca40d652eaa
Hasnain <hashamim@gmail.com>
2019-07-08 19:23:11 -0400

    attempting to fix copy to clipboard bug

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,15 @@
 function copyFunc(e) {
     e.preventDefault();
-    const body = document.getElementsByTagName("body")[0];
+    const header = document.getElementsByTagName("header")[0];
     var copyText = document.createElement("INPUT");
     // copyText.setAttribute("id", "copyText");
     copyText.setAttribute("value","hashamim@gmail.com")
     // var email = document.createTextNode("hashamim@gmail.com");
     // copyText.appendChild(email);
-    body.appendChild(copyText);
+    header.appendChild(copyText);
     copyText.select();
     document.execCommand("copy");
-    body.removeChild(copyText)
+    header.removeChild(copyText)
     var mailI = document.getElementById("copy-link");
     mailI.setAttribute("aria-label","copied!");
     setTimeout(()=>mailI.setAttribute("aria-label","click to copy email"), 2500)
